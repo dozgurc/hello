126c0c984469ee92f2a1597b7d5b3f2cf9d43985
Ozgur C <dozgurc@hotmail.com>
2015-08-24 16:03:32 +0300

    github initial commit

---
+++ deneme1/src/main/java/com/ozgur/deneme/deneme1/App.java
@@ -0,0 +1,15 @@
+package com.ozgur.deneme.deneme1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	public String sayHello() {
+		return "Hello";
+	}
+}
